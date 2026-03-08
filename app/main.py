5a457be7fb41e25aaaa1cab1e4e56698dd033501
Soufiane Charkaoui <soufianecharkaoui98@gmail.com>
2026-03-08 19:29:52 +0000

    feat: implementing the echo builtin

--- app/main.py
+++ app/main.py
@@ -6,10 +6,16 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = sys.stdin.readline().strip()
+        parts = command.split(None, 1)
         if not command:
             continue
         elif command == "exit":
             break
+        elif command.split()[0] == "echo":
+            sys.stdout.write(f"{parts[1]}\n")
+            sys.stdout.flush()
+            continue
+
         sys.stderr.write(f"{command}: command not found \n")
         sys.stderr.flush()
 
